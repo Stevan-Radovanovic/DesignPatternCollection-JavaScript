ed140b071b1e49ea0f242c68be09ecc64e6a6ac5
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-11 23:46:51 +0200

    Constructor Design Pattern Commit v2.0

--- ConstructorDesignPattern/Player.js
+++ ConstructorDesignPattern/Player.js
@@ -16,5 +16,22 @@ class Player {
 
 }
 
-let player1 = new Player("Yaya","Toure",7)
+let player1 = new Player("Yaya","Toure",42)
 player1.toString()
+
+/*
+Constructor Design Pattern
+The same thing can be made with JS Objects
+*/
+let PlayerObj = function(firstName,lastName,number) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.number = number
+}
+
+PlayerObj.prototype.toString = function() {
+    console.log(this.firstName + " " + this.lastName + " " + this.number) 
+}
+
+let p1 = new PlayerObj("Sergio","Aguero",10)
+p1.toString()
